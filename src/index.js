c7801b97f13fd4e9169e17886f488c814ea9ca69
Srr233 <Sr233@yandex.ru>
2020-10-09 10:44:40 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString().split('').reverse();
+
+  if (str[str.length - 1] !== '-') {
+      return +str.join('');
+  } else {
+      return +`${str.slice(0, str.length - 1).join('')}`;
+  }
 }
